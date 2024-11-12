f42e6e50231fb0c5556ec3d82dc452fc5a52b051
DaniAkash <s.daniakash@gmail.com>
2024-11-12 20:55:44 +0530

    feat: setup simple transition

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./**/*.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./**/*.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("tailwindcss-view-transitions")],
+};
